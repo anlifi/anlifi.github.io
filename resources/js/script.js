2051b5d8ebacb8f05ac7f04a37701aa1028140fb
anlifi <72402016+anlifi@users.noreply.github.com>
2020-11-13 10:57:26 +0100

    Set colors and add dark mode toggle switch

---
+++ resources/js/script.js
@@ -0,0 +1,26 @@
+// Toggle switch for light and dark mode
+const toggleSwitch = document.querySelector('.mode-switch input[type="checkbox"]');
+const currentColorMode = localStorage.getItem('mode') ? localStorage.getItem('mode') : null;
+
+// Check current color mode
+if (currentColorMode) {
+    document.documentElement.setAttribute('color-mode', currentColorMode);
+
+    if (currentColorMode === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+// Switch between modes and store user preference
+function switchMode(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('color-mode', 'dark');
+        localStorage.setItem('mode', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('color-mode', 'light');
+        localStorage.setItem('mode', 'light');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchMode, false);
